569ec993786ecfcad4e4ddce0e6b0cea31479d07
jaychang99 <jaychang99@gmail.com>
2024-05-26 09:04:26 +0900

    feat: generate openapi.json file upon starting dev server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  // write document to root path with name openapi.json
+  fs.writeFileSync('openapi.json', JSON.stringify(document, null, 2));
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(8000);
