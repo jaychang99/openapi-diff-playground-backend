fa106f12172e7bb5987a167431561b01aad00e74
jaychang99 <jaychang99@gmail.com>
2024-05-26 20:08:29 +0900

    test: add request body to PATCH: /cats/:id

--- src/cats/dto/update-cat.dto.ts
+++ src/cats/dto/update-cat.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCatDto } from './create-cat.dto';
+import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCatDto extends PartialType(CreateCatDto) {}
+export class UpdateCatDto extends PartialType(CreateCatDto) {
+  @IsString()
+  @ApiProperty({ description: 'The name of the cat' })
+  name: string;
+
+  @IsNumber()
+  @ApiProperty({ description: 'The age of the cat' })
+  age: number;
+}
