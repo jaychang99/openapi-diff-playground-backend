36edb5e99c52e071d00b3b4dbdf6a3b253c8cf41
jaychang99 <jaychang99@gmail.com>
2024-05-26 09:12:15 +0900

    feat(Cats): add name and age values to POST: /cats

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCatDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  @ApiProperty()
+  name: string;
+
+  @IsNumber()
+  @ApiProperty()
+  age: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
